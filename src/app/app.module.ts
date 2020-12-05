ed5ebb4ee112a5f634c28f0a2521077c9c946b67
=prafullkumar123 <=>
2020-12-05 22:15:25 +0530

    add mobiles modual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MobilesComponent } from './mobiles/mobiles.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MobilesComponent
   ],
   imports: [
     BrowserModule,
